e8ad7ee0860645f01bde36a2d0914a2484b73573
Maciej Rapacki <mac.rapacki@gmail.com>
2020-05-28 19:35:46 +0100

    Updates

---
+++ precache-manifest.ac93bcfa79c3d5573af8ce0a63d6bd5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de786d961c65f6d300654152abc30bf4",
+    "url": "/classic-to-do---react/index.html"
+  },
+  {
+    "revision": "154125f6bf5b78fd270c",
+    "url": "/classic-to-do---react/static/css/main.e6b28211.chunk.css"
+  },
+  {
+    "revision": "ded751803023ea0bee93",
+    "url": "/classic-to-do---react/static/js/2.fbf7ca3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/classic-to-do---react/static/js/2.fbf7ca3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "154125f6bf5b78fd270c",
+    "url": "/classic-to-do---react/static/js/main.1f4cccf4.chunk.js"
+  },
+  {
+    "revision": "c3da317fa3c4ad9ab89d",
+    "url": "/classic-to-do---react/static/js/runtime-main.ca231b17.js"
+  }
+]);
